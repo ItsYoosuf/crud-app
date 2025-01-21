3d4620ea58bc7e53f780aa3476e6c22bf91708c5
Yoosuf Mohammed Shaheed <77971881+ItsYoosuf@users.noreply.github.com>
2025-01-21 20:24:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store';
 import App from './App';
 
